658fa742b60bbed3d931ce03cc99049617124729
Agustin709 <aagustin.acosta95@gmail.com>
2022-11-17 23:17:52 -0300

    prueba con post

--- ServidorAPIREST/src/java/Services/Espectaculos.java
+++ ServidorAPIREST/src/java/Services/Espectaculos.java
@@ -6,9 +6,15 @@
 package Services;
 
 import DTOs.EspectadorDto;
+import com.google.gson.Gson;
 import java.util.Date;
+import javax.validation.Valid;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.POST;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,15 +28,28 @@ public class Espectaculos {
     
     @GET
     @Path("/")
-    public Response getTexto(@QueryParam("texto") String texto) {
+    public Response getTexto(@PathParam("texto") String texto) {
         try {
                 
             EspectadorDto usuario = new EspectadorDto(texto, "Edison", "Cavani", "edi@gmail.com", new Date(), -1, "123");  
-            return Response.ok(usuario, MediaType.APPLICATION_JSON).build();
+            return Response.ok(new Gson().toJson(usuario), MediaType.APPLICATION_JSON).build();
 
         } catch (Exception e) {
             System.out.println("Services.Espectaculo.getTexto()" + e.toString());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
+    
+    @POST
+    @Path("/")
+     public Response getDatos (@Valid EspectadorDto dato){
+         try {
+             //EspectadorDto espectaculo = new EspectadorDto ("lucho9","Luis","Suarez","lucho@gmail.com",new Date(), -2, "123");
+             return Response.ok(new Gson().toJson(dato), MediaType.APPLICATION_JSON).build();
+         } catch (Exception e) {
+             System.out.println("Services.Espectaculo.getDatos()" + e.toString());
+             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+         }
+     }
+   
 }
